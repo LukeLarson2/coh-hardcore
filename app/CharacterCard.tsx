6fb707ec5cd00c8cfa0dfcd34e77e6662cca8e35
LukeLarson2 <lucas.m.larson2@gmail.com>
2024-11-14 07:36:50 -0500

    modified to be mobile friendly

--- app/CharacterCard.tsx
+++ app/CharacterCard.tsx
@@ -108,156 +108,158 @@ const CharacterCard = ({ props }: { props: Character }) => {
   return (
     <div
       key={id}
-      className="rounded flex flex-col w-fit min-w-64 h-fit transition-all duration-300 border-4 border-gray-200"
+      className="flex w-full h-full place-content-center place-items-center"
     >
-      <div className={`${getColor()} p-2 rounded-t relative`}>
-        <button
-          onClick={() => handleDelete()}
-          className="absolute bottom-3 right-10 hover:opacity-75 transition-all duration-300"
-        >
-          <FaTrashCan className="size-5" />
-        </button>
-        <button
-          onClick={() => handleOpenModal(true)}
-          className="absolute bottom-3 right-3 hover:opacity-75 transition-all duration-300"
-        >
-          <MdOutlineModeEditOutline className="size-5" />
-        </button>
-        <div className="flex place-items-center w-full h-full space-x-2">
-          {/* eslint-disable-next-line @next/next/no-img-element*/}
-          <img src={getArchIcon()} alt={arch} className="w-6 h-6" />
-          <p className="font-bold text-2xl text-left flex justify-start">
-            {name}
-          </p>
-        </div>
-        <p className="font-light text-sm">
-          Level {level} - {origin} - {arch}
-        </p>
-        <p className="font-light text-sm">{player}</p>
-      </div>
-      <div className="p-2 my-5">
-        <p className="font-bold text-lg">Primary</p>
-        <p>{primary}</p>
-        <p className="font-bold text-lg">Secondary</p>
-        <p>{secondary}</p>
-      </div>
-      <div className="p-2 bg-gray-900 rounded-b">
-        <p className="font-bold text-lg">Revives Available</p>
-        <p>{revives}</p>
-      </div>
-
-      {/* Edit Modal */}
-      {modal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-          <div className="bg-black p-6 rounded shadow-lg max-w-md w-full">
-            <h2 className="text-xl font-bold mb-4">Edit Character</h2>
-            <form className="space-y-4">
-              <input
-                type="text"
-                name="name"
-                value={characterData.name}
-                onChange={handleChange}
-                placeholder="Character Name"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <input
-                type="number"
-                name="level"
-                value={characterData.level}
-                onChange={handleChange}
-                placeholder="Level"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <input
-                type="text"
-                name="origin"
-                value={characterData.origin}
-                onChange={handleChange}
-                placeholder="Origin"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <select
-                name="arch"
-                value={characterData.arch}
-                onChange={handleChange}
-                className="w-full border p-2 rounded bg-gray-900 text-white"
-              >
-                <option value="" disabled>
-                  Select Archetype
-                </option>
-                {archetypes.map((type) => (
-                  <option key={type} value={type}>
-                    {type}
-                  </option>
-                ))}
-              </select>
-              <input
-                type="text"
-                name="primary"
-                value={characterData.primary}
-                onChange={handleChange}
-                placeholder="Primary Power"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <input
-                type="text"
-                name="secondary"
-                value={characterData.secondary}
-                onChange={handleChange}
-                placeholder="Secondary Power"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <input
-                type="number"
-                name="revives"
-                value={characterData.revives}
-                onChange={handleChange}
-                placeholder="Revives"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <select
-                name="color"
-                value={characterData.color}
-                onChange={handleChange}
-                className="w-full border p-2 rounded bg-gray-900 text-white"
-              >
-                <option value="red">Red</option>
-                <option value="blue">Blue</option>
-                <option value="orange">Orange</option>
-                <option value="green">Green</option>
-                <option value="purple">Purple</option>
-                <option value="gray">Gray</option>
-                <option value="black">Black</option>
-              </select>
-              <input
-                type="text"
-                name="player"
-                value={characterData.player}
-                onChange={handleChange}
-                placeholder="Player Name"
-                className="w-full border p-2 rounded bg-gray-900"
-              />
-              <div className="flex justify-between mt-4">
-                <button
-                  type="button"
-                  onClick={() => handleOpenModal(false)}
-                  className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="button"
-                  onClick={handleEdit}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                  disabled={loading}
-                >
-                  {loading ? "Saving..." : "Save Changes"}
-                </button>
-              </div>
-            </form>
+      <div className="rounded flex flex-col w-fit min-w-64 h-fit transition-all duration-300 border-4 bg-black border-gray-200 ">
+        <div className={`${getColor()} p-2 rounded-t relative`}>
+          <button
+            onClick={() => handleDelete()}
+            className="absolute bottom-3 right-10 hover:opacity-75 transition-all duration-300"
+          >
+            <FaTrashCan className="size-5" />
+          </button>
+          <button
+            onClick={() => handleOpenModal(true)}
+            className="absolute bottom-3 right-3 hover:opacity-75 transition-all duration-300"
+          >
+            <MdOutlineModeEditOutline className="size-5" />
+          </button>
+          <div className="flex place-items-center w-full h-full space-x-2">
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
+            <img src={getArchIcon()} alt={arch} className="w-6 h-6" />
+            <p className="font-bold text-2xl text-left flex justify-start">
+              {name}
+            </p>
           </div>
+          <p className="font-light text-sm">
+            Level {level} - {origin} - {arch}
+          </p>
+          <p className="font-light text-sm">{player}</p>
         </div>
-      )}
+        <div className="p-2 my-5">
+          <p className="font-bold text-lg">Primary</p>
+          <p>{primary}</p>
+          <p className="font-bold text-lg">Secondary</p>
+          <p>{secondary}</p>
+        </div>
+        <div className="p-2 bg-gray-900 rounded-b">
+          <p className="font-bold text-lg">Revives Available</p>
+          <p>{revives}</p>
+        </div>
+
+        {/* Edit Modal */}
+        {modal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+            <div className="bg-black p-6 rounded shadow-lg max-w-md w-full">
+              <h2 className="text-xl font-bold mb-4">Edit Character</h2>
+              <form className="space-y-4">
+                <input
+                  type="text"
+                  name="name"
+                  value={characterData.name}
+                  onChange={handleChange}
+                  placeholder="Character Name"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <input
+                  type="number"
+                  name="level"
+                  value={characterData.level}
+                  onChange={handleChange}
+                  placeholder="Level"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <input
+                  type="text"
+                  name="origin"
+                  value={characterData.origin}
+                  onChange={handleChange}
+                  placeholder="Origin"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <select
+                  name="arch"
+                  value={characterData.arch}
+                  onChange={handleChange}
+                  className="w-full border p-2 rounded bg-gray-900 text-white"
+                >
+                  <option value="" disabled>
+                    Select Archetype
+                  </option>
+                  {archetypes.map((type) => (
+                    <option key={type} value={type}>
+                      {type}
+                    </option>
+                  ))}
+                </select>
+                <input
+                  type="text"
+                  name="primary"
+                  value={characterData.primary}
+                  onChange={handleChange}
+                  placeholder="Primary Power"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <input
+                  type="text"
+                  name="secondary"
+                  value={characterData.secondary}
+                  onChange={handleChange}
+                  placeholder="Secondary Power"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <input
+                  type="number"
+                  name="revives"
+                  value={characterData.revives}
+                  onChange={handleChange}
+                  placeholder="Revives"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <select
+                  name="color"
+                  value={characterData.color}
+                  onChange={handleChange}
+                  className="w-full border p-2 rounded bg-gray-900 text-white"
+                >
+                  <option value="red">Red</option>
+                  <option value="blue">Blue</option>
+                  <option value="orange">Orange</option>
+                  <option value="green">Green</option>
+                  <option value="purple">Purple</option>
+                  <option value="gray">Gray</option>
+                  <option value="black">Black</option>
+                </select>
+                <input
+                  type="text"
+                  name="player"
+                  value={characterData.player}
+                  onChange={handleChange}
+                  placeholder="Player Name"
+                  className="w-full border p-2 rounded bg-gray-900"
+                />
+                <div className="flex justify-between mt-4">
+                  <button
+                    type="button"
+                    onClick={() => handleOpenModal(false)}
+                    className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleEdit}
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    disabled={loading}
+                  >
+                    {loading ? "Saving..." : "Save Changes"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
